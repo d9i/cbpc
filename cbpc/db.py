2dbce3e197b107858db2f71a483c22645066cc8a
Dara Kharabi <kharabid@gmail.com>
2021-03-07 12:59:09 -0500

    added connect_base

--- cbpc/db.py
+++ cbpc/db.py
@@ -12,6 +12,34 @@ from os.path import abspath
 from flask import current_app, g
 
 
+def connect_base(path=None):
+    """
+    DB connection wrapper.
+    """
+    if path is None:
+        path = current_app.config["DB_PATH"]
+
+    cxn = sqlite3.connect(path,
+                          detect_types=sqlite3.PARSE_DECLTYPES
+                          )
+
+    sqlite3.register_adapter(uuid.UUID, lambda id: id.bytes_le)
+    sqlite3.register_converter('UUID', lambda bytes: uuid.UUID(bytes_le=bytes))
+
+    return cxn
+
+
+def connect(path=None):
+    """
+    DB connection wrapper wrapper.
+    Reuses one connection thoughout the lifetime of the request.
+    """
+    if "cxn" not in g:
+        g.cxn = connect_base(path)
+
+    return g.cxn
+
+
 def setup_initial(path):
     """Create initial schema"""
 
@@ -22,11 +50,7 @@ def setup_initial(path):
         pass
 
     # Can't use connect() because application context doesn't exist here.
-    # Not registering adapters/converters for this query because no
-    # inserts/selects are being used.
-    cxn = sqlite3.connect(path,
-                          detect_types=sqlite3.PARSE_DECLTYPES
-                          )
+    cxn = connect_base(path)
 
     with cxn:
         cxn.execute('''CREATE TABLE contacts (cid UUID, date TIMESTAMP)''')
@@ -38,35 +62,12 @@ def flush(path):
     """Completely empties the DB - for testing."""
 
     # Can't use connect() because application context doesn't exist here.
-    # Not registering adapters/converters for this query because no
-    # inserts/selects are being used.
-    cxn = sqlite3.connect(path,
-                          detect_types=sqlite3.PARSE_DECLTYPES
-                          )
+    cxn = connect_base(path)
 
     with cxn:
         cxn.execute('''DROP TABLE IF EXISTS contacts''')
 
     cxn.close()
-
-
-def connect(path=None):
-    """
-    DB connection wrapper.
-    Reuses one connection thoughout the lifetime of the request.
-    """
-    if path is None:
-        path = current_app.config["DB_PATH"]
-
-    if "cxn" not in g:
-        g.cxn = sqlite3.connect(path,
-                                detect_types=sqlite3.PARSE_DECLTYPES
-                                )
-
-        sqlite3.register_adapter(uuid.UUID, lambda id: id.bytes_le)
-        sqlite3.register_converter('UUID', lambda bytes: uuid.UUID(bytes_le=bytes))
-
-    return g.cxn
 
 
 def close_cxn(e=None):
@@ -87,5 +88,5 @@ def app_init(app, suppress_db_init=False) -> None:
     app.config["DB_PATH"] = path
 
     if not os.path.exists(path) and not suppress_db_init:
-        print("Database not found. Initializing...")
+        print("Database not found. Initializing at path", path)
         setup_initial(path)
